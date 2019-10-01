3b002b970668708872267420234753be8e9df713
Courtney Pacheco <cpacheco@cpacheco.bos.csb>
2019-10-01 05:59:28 -0400

    Delete PVC first, then try to delete PV.

--- TensorFlow/OpenShift/setup/volumes/force_pv_and_pvc_deletion.sh
+++ TensorFlow/OpenShift/setup/volumes/force_pv_and_pvc_deletion.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-# This script forces a PV and PVC deletion when OpenShift hangs
-oc patch pv nvidia-packages-pv -p '{"metadata":{"finalizers":null}}'
+#################################################################
+# This script forces a PV and PVC deletion when OpenShift hangs #
+#################################################################
+
+# Patch the PVC
 oc patch pvc nvidia-packages-pvc -p '{"metadata":{"finalizers":null}}'
+
+# Delete the PVC
+oc delete pvc nvidia-packages-pvc
+
+# Patch the PV
+oc patch pv nvidia-packages-pv -p '{"metadata":{"finalizers":null}}'
+
+# Sleep
+echo "Sleeping for 10 seconds..."
+sleep 10
+
+# Now delete the PV
+oc delete pv nvidia-packages-pv
