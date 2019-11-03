b18d31674fa165b41243bab6bc2768aca3a3dea6
Courtney Pacheco <cpacheco@cpacheco.bos.csb>
2019-11-03 05:14:34 -0500

    Updating RHEL 7 Dockerfiles to include the new 'install_python_from_source.sh'. Also fixing 'install_ansible_from_source.sh' and including that again. Finally, fixing /etc/subuid and /etc/subgid so that the s2i image commit won't fail.

--- helper_scripts/Ansible/install_ansible_from_source.sh
+++ helper_scripts/Ansible/install_ansible_from_source.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
 ANSIBLE_GIT_VERSION=$1
-RELEASE=$(cat /etc/redhat-release)
+RELEASE=$(cat /etc/redhat-release | cut -d' ' -f 6)
 RHEL_VERSION=
 
 # Get RHEL version from kernel version
-if [[ ${RELEASE} == *"7."* ]]; then
+if [[ ${RELEASE} == "7."* ]]; then
     RHEL_VERSION=7
-elif [[ ${RELEASE} == *"8."* ]]; then
+elif [[ ${RELEASE} == "8."* ]]; then
     RHEL_VERSION=8
 else
-    echo "ERROR. Unrecognized RHEL version. Exiting now."
+    echo "ERROR. Unrecognized RHEL version '${RELEASE}'. Exiting now."
     exit 1
 fi
 
-# If the version of RHEL is 7.x, then install EPEL to install python36. Also install git
+# This script assumes Python has been installed for RHEL 7
+python3_err_msg="ERROR. Python 3.6 is not installed. Please install Python 3.6 using ../Python/install_python_from_source.sh"
 if [[ ${RHEL_VERSION} == 7 ]]; then
-    yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-    yum -y install git python34 python34-devel
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3.4 get-pip.py
-    rm get-pip.py
+
+    # Check for the Python 3 executables
+    if [[ -x /usr/bin/python3 ]] && [[ -x /usr/local/bin/python3 ]]; then
+        echo ${python3_err_msg}
+        exit 1
+    else 
+        python3_minor_version=$(python3 --version | cut -d' ' -f 2 | cut -d'.' -f 2)
+        if (( python3_minor_version < 6 )); then
+	    echo ${python3_err_msg}
+	    exit 1
+        fi
+    fi
+
+    yum -y install git
 
 # Else, install python36 via dnf
 else
@@ -35,16 +45,17 @@ cd ansible
 git checkout tags/${ANSIBLE_GIT_VERSION}
 
 # Install requirements via pip
-if [[ "${RHEL_VERSION}" == "7" ]]; then
-    pip install -r requirements.txt
-else
-    pip3 install -r requirements.txt
+pip3 install -r requirements.txt
+
+# Set Python executable for RHEL 7
+if [[ ${RHEL_VERSION} == 7 ]]; then
+    sed -i "s/PYTHON=python/PYTHON=python3/g" Makefile && \
 fi
 
 # Setup Ansible
 if [[ "${RHEL_VERSION}" == "7" ]]; then
-    python3.4 setup.py build
-    python3.4 setup.py install
+    python3 setup.py build
+    python3 setup.py install
 else
     python3.6 setup.py build
     python3.6 setup.py install
